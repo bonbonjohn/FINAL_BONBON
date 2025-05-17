0e2c7d4a70225fadf39282971a1a7af52c71eb1b
unknown <HH808@ccs.local>
2025-05-17 18:47:16 +0800

    final

--- frontend/e2e/protractor.conf.js
+++ frontend/e2e/protractor.conf.js
@@ -16,7 +16,7 @@ exports.config = {
     browserName: 'chrome'
   },
   directConnect: true,
-  baseUrl: 'https://intprog-final-backend.onrender.com',
+  baseUrl: 'https://final-bonbon.onrender.com',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
